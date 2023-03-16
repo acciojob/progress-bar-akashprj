76cc3ca2a885063a339c2317389427e47e049464
Akash Kumar Prajapati <91784572+akashprj@users.noreply.github.com>
2023-03-16 23:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const circles = document.querySelectorAll('.circle');
+
+let currentActive = 1;
+
+nextBtn.addEventListener('click', () => {
+  currentActive++;
+  if (currentActive > circles.length) {
+    currentActive = circles.length;
+  }
+  updateProgress();
+});
+
+prevBtn.addEventListener('click', () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  updateProgress();
+});
+
+function updateProgress() {
+  circles.forEach((circle, idx) => {
+    if (idx < currentActive) {
+      circle.classList.add('active');
+    } else {
+      circle.classList.remove('active');
+    }
+  });
+
+  if (currentActive === 1) {
+    prevBtn.disabled = true;
+  } else if (currentActive === circles.length) {
+    nextBtn.disabled = true;
+  } else {
+    prevBtn.disabled = false;
+    nextBtn.disabled = false;
+  }
+}
